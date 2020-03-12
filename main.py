e2146376538c5d9c24506256aab35324b0232217
Aman Zishan M.A <amanzishan.az@gmail.com>
2020-03-12 17:02:20 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pytesseract
 import cv2
-img = cv2.imread('img/test.jpeg')
+img = cv2.imread('img/YOUR_FILE_NAME.EXTENSION')
 text = pytesseract.image_to_string(img)
 print(text)
 f = open("sample.txt", "a")
